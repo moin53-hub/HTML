a86c828f98095940ef3b28895a6e762175fae32a
moin53-hub <shaikmoin632@gmail.com>
2021-06-09 15:48:04 +0530

    9Feb2021

---
+++ 9Feb2021/seq.js
@@ -0,0 +1,30 @@
+
+/*
+var age = 18;
+if (age > 18)
+{
+    console.log("you are eligible to vote");
+}
+else
+{
+    console.log("you are not ellibile to vote");
+}*/
+
+/*if(2)//false  if (1) true other 0 all are true //if we give string also "hello" find the num gives true
+{
+    console.log("true");
+}
+else
+{
+    console.log("false");
+}*/
+
+var choice="mech";
+
+switch(choice){
+    case "cse":console.log("you selected cse"); break;
+    case "eee":console.log("you selected eee"); break;
+    case "mech":console.log("you selected mech"); break;
+
+    default:console.log("this course is not available");
+}
